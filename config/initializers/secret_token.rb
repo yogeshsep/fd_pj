1c01c0a6b71aecddef0837ab32040da757c92a5e
yogeshsep <yogeshsep15@gmail.com>
2014-05-05 10:56:26 +0530

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FdPj::Application.config.secret_token = 'f98979bda30e65a795157b4e05178891f5751f41daf4911da30b1feb1d5962184cca16e9fa77b57caa4121d53a370d9eefd6791be112ebbde81571cdb9023904'
